be80b3548438593f313d1bc365773713650f5985
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-03-16 09:41:53 +0530

    feat: Add mock JSON data for runner

---
+++ src/mockData.js
@@ -0,0 +1,9 @@
+export const runnerData = {
+  "runner": "Rahul Sharma",
+  "challenge": "IRL Season 1",
+  "tier": "RoadWarrior",
+  "target_distance": 100,
+  "completed_distance": 62,
+  "weekly_runs": [5,8,0,6,7],
+  "rank": 18
+};
